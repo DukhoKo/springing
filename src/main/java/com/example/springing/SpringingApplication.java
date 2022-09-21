16136baa241cccd2e9bf8e689e1761e49988f88c
kodukho <yhdh4317@gmail.com>
2022-09-21 23:47:47 +0900

    SpringBootConfiguration

--- src/main/java/com/example/springing/SpringingApplication.java
+++ src/main/java/com/example/springing/SpringingApplication.java
@@ -1,13 +1,25 @@
 package com.example.springing;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@SpringBootConfiguration
+//@ComponentScan
+//@EnableAutoConfiguration
 public class SpringingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringingApplication.class, args);
+
+		SpringApplication application = new SpringApplication(SpringingApplication.class);
+		application.setWebApplicationType(WebApplicationType.NONE);
+		application.run(args);
+
+	//	SpringApplication.run(SpringingApplication.class, args);
 	}
 
 }
